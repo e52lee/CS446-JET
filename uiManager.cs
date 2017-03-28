955175df72781250d060173c07800b13dd8e240c
e52lee <e52lee@uwaterloo.ca>
2017-03-28 18:09:25 -0400

    Added current speed

---
+++ uiManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class uiManager : MonoBehaviour {
+
+	// Use this for initialization
+	public Text scoreText;
+	public Text hiScoreText;
+	public Text speedText;
+	bool gameOver;
+	int score;
+	int hiScore;
+	int speed;
+
+	
+	/**
+	 * Initialization
+	 */
+	void Start () {
+		gameOver = false;
+		score = 0;
+		speed = 0;
+		if(!PlayerPrefs.HasKey("HighScore") != null){
+			hiScore =  PlayerPrefs.GetInt("HighScore");
+			hiScoreText.text = "High Score: " + hiScore;
+		}
+		else {
+			//hiScore = 0;
+			hiScoreText.text = "High Score: 0";
+		}
+		InvokeRepeating("scoreUpdate", 1.0f, 0.5f);
+		
+		speed = (int) GameObject.Find("SpeedController").GetComponent<SpeedController>().GetDisplaySpeed();
+	}
+	
+	/**
+	 * Update is called once per frame
+	 */
+	void Update () {
+		scoreText.text = "Score: " + score;
+		speed = (int) GameObject.Find("SpeedController").GetComponent<SpeedController>().GetDisplaySpeed();
+		speedText.text = "Speed: " + speed;
+		//hiScoreText.text = "High Score: " + hiScore;
+	}
+
+	void scoreUpdate () {
+		if(!gameOver){
+			score += 1;
+		}
+		if(score > hiScore){
+			//hiScore = score;
+			PlayerPrefs.SetInt("HighScore", score);
+		}
+	}
+
+	public void gameOverActivated(){
+		gameOver = true;
+
+		//PlayerPrefs.DeleteAll();
+	}
+}
